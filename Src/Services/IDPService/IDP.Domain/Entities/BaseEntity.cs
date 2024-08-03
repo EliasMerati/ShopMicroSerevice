36a6120c188b1b2d02cb3035d07851fa34d63967
elias.merati <elias.merati@gmail.com>
2024-08-03 15:53:34 +0330

    Create Base Controller & Base Entity

---
+++ Src/Services/IDPService/IDP.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IDP.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            InsertDate = DateTime.UtcNow;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public DateTime InsertDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
